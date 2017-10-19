66a713c955e4909feedbcf7c5be7a76e8e2be99c
jpjoshua <jp.joshuaparker@gmail.com>
2017-10-19 15:03:22 -0500

    login is not old index.html and a new index.html is main in package.json
    -renamed index to login
    -made new index.html
    -package.json now uses the new index as main
    - will code a window to appear on startup to login and gain access to S.M.S
    -

---
+++ njs_sdk/switch_to_index.js
@@ -0,0 +1,15 @@
+var loginButton = document.getElementById("login-button").clicked;
+var userId = ["jcp0048", "testUser"]
+var userIdValue = document.getElementById("user-id-input").value
+
+function checkId() {
+  if (loginButton == true) {
+    if () {
+
+    } else {
+      block of code to be executed
+      if the condition is false
+    }
+  }
+}
+  button.addEventListener("enter", change)
