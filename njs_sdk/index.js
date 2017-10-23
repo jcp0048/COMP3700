0f76f6f5bfeb92480dfdc30ffa949f7db359a348
jpjoshua <jp.joshuaparker@gmail.com>
2017-10-23 15:57:48 -0500

    db added

--- njs_sdk/index.js
+++ njs_sdk/index.js
@@ -1,4 +1,11 @@
+var SQL = require('sql.js');
+var fs = require('fs');
 $(document).ready(function() {
+  var test = connect_to_db();
+  console.log(test);
+  var table = get_table(test, "items");
+  console.log(table);
+  console.log(table[0].values[0][0]);
   $('#order-id-input').bind('keydown', function(event) {
     console.log(event.keyCode);
     switch (event.keyCode) {
@@ -7,31 +14,20 @@ $(document).ready(function() {
   });
 })
 
-var mysql = require('mysql')
-var con = mysql.createConnection({
-  //Propteries
-  multipleStatements: true,
-  host: "localhost",
-  user: "comp3700",
-  password: "root",
-  database: "mydb"
-});
-
-function createDb(){
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    /*Create a database named "mydb":*/
-    con.query("CREATE DATABASE mydb", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-  });
-};
+function connect_to_db() {
+  databasePath = "./mydb.db"
+   db = new SQL.Database(fs.readFileSync(databasePath));
+   return db;
+}
 
 function db_insert(connection) {
 
 };
+
+function get_table(db, table) {
+  var table = db.exec("SELECT * FROM " + table);
+  return table;
+}
 
 
 function showOrderId() {
